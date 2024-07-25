fc8325bafeba9bbd1c6b5c4d1916639073efc3d9
Tư Lê <112106266+1612770@users.noreply.github.com>
2024-07-25 11:13:23 +0700

    update(file): app (#1)
    Co-authored-by: Lê Văn Tư <tu.le@pito.vn>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { App as AntdApp, ConfigProvider } from "antd";
+import "antd/dist/reset.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "Inter",
+        },
+        components: {
+          Collapse: {
+            contentPadding: 0,
+          },
+        },
+      }}
+    >
+      <AntdApp>
+        <Component {...pageProps} />
+      </AntdApp>
+    </ConfigProvider>
+  );
 }
