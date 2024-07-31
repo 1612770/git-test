61946c047f78305efec19f48c71e1cea5d897402
Lê Văn Tư <tu.le@pito.vn>
2024-07-31 13:20:31 +0700

    update(test): sha

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    VERCEL_GIT_COMMIT_SHA: process.env.VERCEL_GIT_COMMIT_SHA,
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {process.env.VERCEL_GIT_COMMIT_SHA}
       <Component {...pageProps} />
     </>
   );
