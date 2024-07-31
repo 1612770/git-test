10cf7428b65974cfecef64cd663719a21b4fc0fe
Lê Văn Tư <tu.le@pito.vn>
2024-07-31 11:45:48 +0700

    update(test): VERCEL_GIT_COMMIT_SHA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {process.env.VERCEL_GIT_COMMIT_SHA}
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ vercel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_SHA"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "release" || "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "develop" ]]; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0
+fi
